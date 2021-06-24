a2d93aac7babbc89d6885f4e804a57d7e4247370
Vincent Le <quyen.lechi@gmail.com>
2021-06-23 18:30:20 -0700

    Init

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const { join } = require('path');
+const { readFileSync } = require('fs');
+const { PrismaClient } = require('@prisma/client');
+const { getUserId } = require('./utils');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
+const Vote = require('./resolvers/Vote');
+
+const prisma = new PrismaClient();
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs: readFileSync(join(__dirname, 'schema.graphql'), 'utf-8'),
+  resolvers: {
+    Query,
+    Mutation,
+    Subscription,
+    User,
+    Link,
+    Vote,
+  },
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
+  },
+});
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url}`);
+});
